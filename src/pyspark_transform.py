d03c54797929e35bbb08a8af8494cfa8d70dc23a
Gowthaman K <gowthaman.k0411@gmail.com>
2026-06-09 18:22:23 +0530

    Initialize PySpark session and read CSV file

--- src/pyspark_transform.py
+++ src/pyspark_transform.py
@@ -1,1 +1,9 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("PySparkETL").getOrCreate()
+
+df = spark.read.csv("input.csv", header=True)
+
+df.show()
+
+spark.stop()
